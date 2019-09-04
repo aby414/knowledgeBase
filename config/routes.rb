4a50c7aa8d7791d4f41d1645960a7865ca65d533
aby414 <agin.punneliparambil@haw-hamburg.de>
2019-09-04 16:50:33 +0200

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #  Creating a route to the index page
+  root 'articles#index', as: 'articles'
+
+  resources :articles
+end
