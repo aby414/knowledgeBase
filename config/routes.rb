54addaabca86e84af061eb2104fcd17a0015a860
aby414 <agin.punneliparambil@haw-hamburg.de>
2019-09-04 18:06:39 +0200

    edit articled added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   #  Creating a route to the index page
-  root 'articles#index', as: 'articles'
+  root 'articles#index', as: 'home'
 
   resources :articles
 end
