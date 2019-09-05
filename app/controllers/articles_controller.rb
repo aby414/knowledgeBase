6e2edf3320fa4049dd8e4e086b3bc25b3ba81b31
aby414 <agin.punneliparambil@haw-hamburg.de>
2019-09-05 18:25:45 +0200

    translation to german added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
 
-    if(@article.update(article_params))
+    if (@article.update(article_params))
       redirect_to @article
     else
       render 'new'
@@ -39,8 +39,8 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-
-    if(@article.save)
+    @article.body = translate_to_ger(params[:article][:body])
+    if (@article.save)
       redirect_to @article
     else
       render 'edit'
@@ -50,5 +50,36 @@ class ArticlesController < ApplicationController
 
   private def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  #translate text to german
+  # source: https://github.com/MicrosoftTranslator/Text-Translation-API-V3-Ruby/blob/master/Translate.rb
+  private def translate_to_ger(text)
+    subscription_key = $ms_secret_key
+    endpoint = 'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0'
+
+    #translate to German
+    params = '&to=de'
+
+    uri = URI (endpoint + params)
+
+    content = '[{"Text" : "' + text + '"}]'
+
+    request = Net::HTTP::Post.new(uri)
+    request['Content-Type'] = 'application/json'
+    request['Content-length'] = content.length
+    request['Ocp-Apim-Subscription-Key'] = subscription_key
+    request['X-ClientTraceId'] = SecureRandom.uuid
+    request.body = content
+
+    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+      http.request (request)
+    end
+
+    result = response.body.force_encoding("utf-8")
+
+    json_result = JSON.parse(result)
+    trans_res = json_result[0]['translations'][0]['text']
+    return trans_res
   end
 end
---
+++ config/secrets.rb
@@ -0,0 +1,2 @@
+#secret key for MS translator
+$ms_secret_key = '50cff510f6c84ee5a24e87cbcc24cf92'
