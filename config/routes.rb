dbf091348e408b48897d59d4a139387838733a6a
aby414 <agin.punneliparambil@haw-hamburg.de>
2019-09-05 14:06:18 +0200

    faq section(with CRUD) added

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   root 'articles#index', as: 'home'
 
   resources :articles
+  resources :questions
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_04_130009) do
+ActiveRecord::Schema.define(version: 2019_09_05_105110) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.text "answer"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
